4021b52819b902e894c51324611d2a7cc2c2e15b
Devjb001 <suleelijah13@gmail.com>
2024-08-21 11:30:01 +0100

    did hamburger

--- apps.js
+++ apps.js
@@ -1,11 +1,19 @@
-const menuList = document.getElementById("menuList");
+let hamburger = document.querySelector('#bars');
+let cancelX = document.querySelector('#cancel');
+let menuList = document.querySelector('#menuList');
 
-menuList.style.maxHeight ="0px";
+console.log('Elements:', hamburger, cancelX, menuList);
 
-function toggleMenu(){
-    if(menuList.style.maxHeight =="0px"){
-        menuList.style.maxHeight ="400px";
-    }else{
-        menuList.style.maxHeight ="0px";
-    }
-}
+
+hamburger.addEventListener("click", () => {
+    menuList.classList.toggle("active");
+    hamburger.classList.toggle("hidden"); // Add a class to hide the hamburger
+    cancelX.classList.toggle("show"); // Add a class to show the X icon
+  });
+  
+  cancelX.addEventListener("click", () => {
+    menuList.classList.remove("active");
+    hamburger.classList.remove("hidden"); // Remove the class to show the hamburger
+    cancelX.classList.remove("show"); // Remove the class to hide the X icon
+  });
+  
